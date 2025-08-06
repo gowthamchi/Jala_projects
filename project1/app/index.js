93a3fb414da7fd01f2b1e5c4b96fbbbf22980203
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-08-06 14:58:44 +0530

    Update index.js

--- project1/app/index.js
+++ project1/app/index.js
@@ -5,11 +5,10 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Node.js CI/CD via Jenkins! this is gowtham reddy');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
 if (require.main === module) {
-  app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
 }
+
+module.exports = server; // <-- this is important for testing
