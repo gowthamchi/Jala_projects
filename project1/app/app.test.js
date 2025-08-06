95a5b80e7412dbd6e7ccf4eeec90f8e82108e712
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-08-06 14:36:11 +0530

    Update app.test.js

--- project1/app/app.test.js
+++ project1/app/app.test.js
@@ -1,8 +1,9 @@
-const chai = require('chai');
-const expect = chai.expect;
 
-describe("Sample Test", () => {
-  it("should return true", () => {
-    expect(true).to.equal(true);
+const assert = require('assert');
+const app = require('./app'); // Make sure your real code is imported!
+
+describe('Sample Test', () => {
+  it('should return true', () => {
+    assert.strictEqual(true, true);
   });
 });
