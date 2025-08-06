bd10a7e5ef55db74591381f6a2c953d0f27abdfc
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-08-06 14:54:31 +0530

    Update index.js

--- project1/app/index.js
+++ project1/app/index.js
@@ -8,3 +8,8 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
+}
