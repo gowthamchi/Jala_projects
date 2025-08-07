3d7c30f0780c894dec3dec28d228d7db841c9b93
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-08-07 14:46:05 +0530

    Update index.js

--- project1/app/index.js
+++ project1/app/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const client = require('prom-client');
+
 const app = express();
+const port = 3000;
 
 const register = new client.Registry();
 client.collectDefaultMetrics({ register });
 
-app.get('/metrics',async (req,res)=>{
-  register.set('Content-type',register.contentType);
-  register.end(await register.metrics());
+app.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', register.contentType);
+  res.end(await register.metrics());
 });
+
 
 app.get('/', (req, res) => {
   res.send('Default Prometheus metrics enabled.');
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
